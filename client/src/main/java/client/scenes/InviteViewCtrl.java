92443e111f06f7f4ae9de00faabb3d2f86d4f6a5
Calin Andrei Radoi <C.A.Radoi@student.tudelft.nl>
2024-04-10 16:52:17 +0200

    Added button for test emails

--- client/src/main/java/client/scenes/InviteViewCtrl.java
+++ client/src/main/java/client/scenes/InviteViewCtrl.java
@@ -75,7 +75,7 @@ public class InviteViewCtrl implements Initializable {
         }
         server.sendEmail(server.getMailConfig().getUsername(), "Test", "This is a test email. If you see it, then everything works fine!");
     }
-    
+
     private void sendInvite() {
         if (!validEmailConfig) {
             System.out.println("NO CONFIG AVAILABLE");
